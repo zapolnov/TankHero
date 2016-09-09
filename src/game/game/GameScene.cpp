d13dcd9670814d5d7fb144426900536b0f1268e4
Nikolay Zapolnov <zapolnov@gmail.com>
2016-09-09 17:28:13 +0200

    Added music into the game.

--- src/game/game/GameScene.cpp
+++ src/game/game/GameScene.cpp
@@ -1,12 +1,18 @@
 #include "GameScene.h"
 #include "Level.h"
+#include "src/engine/sound/SoundManager.h"
+#include "src/engine/Engine.h"
 
 GameScene::GameScene(Engine* engine, PendingResources& resourceQueue)
     : mEngine(engine)
 {
+    uint16_t music = engine->soundManager()->soundNameId("S31-Unexpected Trouble.ogg");
+    resourceQueue.sounds.emplace(music);
+
     auto level = std::make_shared<Level>(engine, resourceQueue);
-    resourceQueue.custom.emplace_back([this, level]{
+    resourceQueue.custom.emplace_back([this, level, music] {
         level->load("level1.dat");
         setRootNode(level);
+        mEngine->soundManager()->play(music, true);
     });
 }
