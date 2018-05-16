52c24cf761f3e132478cf9b39470c1df67c8e7d2
Nikolay Zapolnov <zapolnov@gmail.com>
2018-05-17 00:41:12 +0200

    Small fix for the script.

--- emscripten.cmd
+++ emscripten.cmd
@@ -7,7 +7,11 @@ if not exist _build.emscripten mkdir _build.emscripten
 if errorlevel 1 goto error
 
 cd _build.emscripten
-cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="%~dp0cmake/emscripten.cmake" ..
+cmake -G "MinGW Makefiles" ^
+    -DCMAKE_BUILD_TYPE=Release ^
+    -DCMAKE_TOOLCHAIN_FILE="%~dp0cmake/emscripten.cmake" ^
+    -DEMSCRIPTEN_ROOT_PATH=D:\emsdk\emscripten\1.37.40 ^
+    ..
 if errorlevel 1 goto error
 
 mingw32-make -j 4
